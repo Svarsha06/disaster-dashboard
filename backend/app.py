d3d07a8d80a757352a1296d9b845bc881924a816
Svarsha06 <sivavarsha06@gmail.com>
2026-02-08 09:38:38 +0530

    Update and rename 01_server.py to app.py

---
+++ backend/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Python app deployed successfully 🚀"
+
+@app.route("/health")
+def health():
+    return jsonify(status="OK", message="Server is running")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
